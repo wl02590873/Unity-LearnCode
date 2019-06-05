ba47f0ce2a8e73faf1c52c66f62b3c5da5432f20
wl02590873 <51389151+wl02590873@users.noreply.github.com>
2019-06-05 11:41:35 +0800

    新增狗類別欄位

---
+++ Unity-LearnCode/Assets/Scripts/Dog.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+public class Dog
+{
+    #region 欄位
+    public string name;
+    public float Weight;
+    public string type;
+    public string sex;
+    public string color;
+    public int age;
+    #endregion
+
+    #region
+    public void Bark()
+    {
+        Debug.Log(name + "汪汪");
+    }
+    #endregion
+}
