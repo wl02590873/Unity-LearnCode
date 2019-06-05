18c09c11c99f1477aab633a4fe31cf9f10e191de
wl02590873 <51389151+wl02590873@users.noreply.github.com>
2019-06-05 15:13:20 +0800

    新增練習2

---
+++ Unity-LearnCode/Assets/練習2/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Soldier SoliderA = new Soldier();
+
+    private void Start()
+    {
+        SoliderA.lv = 30;
+        Debug.Log("士兵經驗-取得:" + SoliderA.GetExp());
+        SoliderA.SetExp(100);
+        Debug.Log("士兵經驗值-設定:" + SoliderA.exp);
+    }
+}
---
+++ Unity-LearnCode/Assets/練習2/Soldier.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Soldier
+{
+    public int exp;
+    public int lv;
+
+    /// <summary>
+    /// 設定經驗值計算方式
+    /// </summary>
+    /// <returns>傳回計算後經驗值</returns>
+    public int GetExp()
+    {
+        exp = lv * 10;
+        return exp;
+    }
+
+    /// <summary>
+    /// 設定經驗值
+    /// </summary>
+    /// <param name="getExp"></param>
+    public void SetExp(int getExp)
+    {
+        exp = getExp;
+    }
+}
