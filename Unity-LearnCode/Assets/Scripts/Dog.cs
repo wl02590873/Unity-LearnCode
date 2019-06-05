b79f65e3e4e0442ad11db3a8450787eaae9597c2
wl02590873 <51389151+wl02590873@users.noreply.github.com>
2019-06-05 13:18:26 +0800

    新增狗吃東西的方法

--- Unity-LearnCode/Assets/Scripts/Dog.cs
+++ Unity-LearnCode/Assets/Scripts/Dog.cs
@@ -11,9 +11,37 @@ public class Dog
     #endregion
 
     #region
+    /// <summary>
+    /// 狗狗叫方法
+    /// </summary>
     public void Bark()
     {
-        Debug.Log(name + "汪汪");
+        Debug.Log(name + ":汪汪");
     }
     #endregion
+
+
+    /// <summary>
+    /// 轉換狗的年齡
+    /// </summary>
+    /// <returns></returns>
+    public int ConverAge()
+    {
+        return age * 7;
+    }
+    public void Eat(string food,string speed = "慢")
+    {
+        Debug.Log(name + ":" + food + "，速度:" + speed);
+    }
+
+    /// <summary>
+    /// 狗狗吃東西的方法
+    /// </summary>
+    /// <param name="food">食物</param>
+    /// <param name="count">份量</param>
+    public void Eat (string food,int count)
+    {
+        Debug.Log(name + ":" + food + "，份量:" + count);
+    }
+
 }
